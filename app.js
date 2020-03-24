4ea73d2314758853063ac51eccf6ed3659c5f1a2
Trina <trinamceary@gmail.com>
2020-03-24 09:00:44 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('dev'));
+
+const data = require('app-data');
+
+
+app.get('/app', (req, res) => {
+ const { sort, genre } = req.query;
+
+ if (!sort && !genre) {
+   return res.json(data)
+ }
+
+ if (sort !== 'rating' && sort !== 'app') {
+   return res
+          .status(400)
+          .send('Sort by rating or app')
+ }
+
+ const genreValues = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card']
+ if (!genreValues.includes(genre)) {
+   return res 
+          .status(400)
+          .send('Select a valid genre')
+ }
+
+ if (sort === 'rating') {
+
+ }
+
+ if (sort ==='app') {
+
+ }
+
+
+
+
+})
